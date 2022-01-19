640a52fd529f91a4eac69a6e305fcd8ac3faab91
soumukhe <soumukhe@cisco.com>
2022-01-19 16:35:33 -0600

    1st push

---
+++ override.tf
@@ -0,0 +1,2 @@
+variable "access_key" { default = "put_correct_aws_access_key" }
+variable "secret_key" { default = "put_correct_aws_secret_key" }
---
+++ vars.tf
@@ -0,0 +1,22 @@
+# this is the file that defines the variables.  The values should be specified in terraform.tfvars
+# variable "region" { description = "enter AWS Region " }
+variable "access_key" { description = "enter Access Key" }
+variable "secret_key" { description = "enter Secret Key" }
+variable "instance_type" { default = "t2.micro" }
+
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "availability_zone1" {
+default = "us-east-1a"
+}
+
+
+
+
+variable "num_inst" {
+  type    = number
+  default = 2
+}
